0f00b1c10cea7983086012d0fa450ab77115f505
henriquefantin <henrique.fantin.ns@gmail.com>
2020-05-24 16:08:02 -0300

    004/007 TG01 24.05.20:16.07

---
+++ database/migrations/2020_05_24_185048_create_funcionario.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFuncionario extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionario', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 45);
+            $table->bigInteger('cpf');
+            $table->bigInteger('rg');
+            $table->string('rua', 50);
+            $table->bigInteger('numero_casa');
+            $table->string('cidade', 55);
+            $table->string('bairro', 55);
+            $table->bigInteger('cep');
+            $table->string('estado', 50);
+            $table->bigInteger('numero_celular');
+            $table->string('email', 200);
+            $table->string('estado_civil', 20);
+            $table->string('usuario', 15);
+            $table->string('senha', 15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('funcionario');
+    }
+}
