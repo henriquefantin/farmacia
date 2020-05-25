d327d1128fcbc356eca59d90266a9584742e69e0
henriquefantin <henrique.fantin.ns@gmail.com>
2020-05-24 23:29:23 -0300

    010 TG01 24.05.20:23.29

---
+++ database/migrations/2020_05_25_021651_create_produto.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProduto extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto', function (Blueprint $table) {
+            $table->id();
+             $table->unsignedBigInteger('id_fornecedor');
+            $table->string('nome', 255);
+            $table->text('descricao');
+            $table->decimal('valor_unitario', 20, 2);
+            $table->string('unidade_venda', 20);
+            
+            $table->foreign('id_fornecedor')->references('id')->on('fornecedor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto');
+    }
+}
