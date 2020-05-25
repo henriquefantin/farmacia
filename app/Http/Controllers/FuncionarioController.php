c22f02b211b07cce2bbc17ef1c2d8aecbf514e68
henriquefantin <henrique.fantin.ns@gmail.com>
2020-05-25 00:46:39 -0300

    032 TG01 25.05.20:00.46

---
+++ app/Http/Controllers/FuncionarioController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Funcionario;
+use Auth;
+
+class FuncionarioController extends Controller
+{
+	function telaCadastro(){
+        return view("");
+    }
+
+    function telaAlteracao($id){
+        $funcionario = Funcionario::find($id);
+        return view("tela_alterar_funcionario", [ "u" => $funcionario ]);
+    }
+
+    function alterar(Request $req, $id){
+       
+        $funcionario = Funcionario::find($id);
+
+        $nome = $req->input('nome');
+        $cpf = $req->input('cpf');
+        $rg = $req->input('rg');
+        $rua = $req->input('rua');
+        $numero_casa = $req->input('numero_casa');
+        $cidade = $req->input('cidade');
+        $bairro = $req->input('bairro');
+        $cep = $req->input('cep');
+    	$estado = $req->input('estado');
+        $numero_celular = $req->input('numero_celular');
+        $email = $req->input('email');
+        $estado_civil = $req->input('estado_civil');
+
+        $funcionario->nome = $nome;
+        $funcionario->cpf = $cpf;
+        $funcionario->rg = $rg;
+        $funcionario->rua = $rua;
+        $funcionario->numero_casa = $numero_casa;
+        $funcionario->cidade = $cidade;
+        $funcionario->bairro = $bairro;
+        $funcionario->cep = $cep;
+        $funcionario->estado = $estado;
+        $funcionario->numero_celular = $numero_celular;
+        $funcionario->email = $email;
+        $funcionario->estado_civil = $estado_civil;
+
+        if ($funcionario->save()){
+            $msg = "Cliente $nome alterado com sucesso.";
+        } else {
+            $msg = "Cliente não foi alterado.";
+        }
+
+        return view("retorno", [ "mensagem" => $msg]);
+        
+    }
+
+    function excluir($id){
+        
+        $funcionario = Funcionario::find($id);
+
+        foreach ($funcionario->vendas as $v){
+            $v->delete();
+        }
+
+        if ($funcionario->delete()){
+            $msg = "Cliente $id excluído com sucesso.";
+        } else {
+            $msg = "Cliente não foi excluído.";
+        }
+
+        return view("retorno", [ "mensagem" => $msg]);
+        
+    }
+
+    function adicionar(Request $req){
+        
+    	$nome = $req->input('nome');
+        $cpf = $req->input('cpf');
+        $rg = $req->input('rg');
+        $rua = $req->input('rua');
+        $numero_casa = $req->input('numero_casa');
+        $cidade = $req->input('cidade');
+        $bairro = $req->input('bairro');
+        $cep = $req->input('cep');
+    	$estado = $req->input('estado');
+        $numero_celular = $req->input('numero_celular');
+        $email = $req->input('email');
+        $estado_civil = $req->input('estado_civil');
+    
+    	$funcionario = new Funcionario();
+    	$funcionario->nome = $nome;
+        $funcionario->cpf = $cpf;
+        $funcionario->rg = $rg;
+        $funcionario->rua = $rua;
+        $funcionario->numero_casa = $numero_casa;
+        $funcionario->cidade = $cidade;
+        $funcionario->bairro = $bairro;
+        $funcionario->cep = $cep;
+        $funcionario->estado = $estado;
+        $funcionario->numero_celular = $numero_celular;
+        $funcionario->email = $email;
+        $funcionario->estado_civil = $estado_civil;
+    	
+    	if ($funcionario->save()){
+    		$msg = "Usuário $nome cadastrado com sucesso.";
+    	}else{
+    		$msg = "Usuário não foi cadastrado.";
+    	}
+
+    	return view("retorno", [ "mensagem" => $msg ]);
+        
+    }
+
+    function listar(){    
+            $funcionario = Funcionario::all();
+            return view("lista", [ "us" => $funcionario ]);
+    }    
+}
