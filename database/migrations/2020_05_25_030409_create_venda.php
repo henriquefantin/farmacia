358c0750910fa06d361d7459757dc22055b4691a
gilsonbcastanheira@gmail.com <gilsonbcastanheira@gmail.com>
2020-05-25 00:14:46 -0300

    009 TG01 25.05.2020:00.14

---
+++ database/migrations/2020_05_25_030409_create_venda.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVenda extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('venda', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_cliente');
+            $table->unsignedBigInteger('id_funcionario');
+            $table->string('descricao_venda', 255);
+            $table->decimal('valor_venda', 20, 2);
+
+            $table->foreign('id_cliente')->references('id')->on('cliente');
+            $table->foreign('id_funcionario')->references('id')->on('funcionario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('venda');
+    }
+}
