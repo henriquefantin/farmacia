4d1ff6cff362f2cc932faf6d28bc7324655fce79
henriquefantin <henrique.fantin.ns@gmail.com>
2020-05-31 15:56:49 -0300

    REPAROS TG01 31.05.20:15.56

--- resources/views/listar_funcionarios.blade.php
+++ resources/views/listar_funcionarios.blade.php
@@ -19,6 +19,7 @@
           <th></th>
           <th></th>
     <tr>
+
       <th>ID</th>
       <th>Nome</th>
       <th>Login</th>
@@ -32,16 +33,16 @@
       <td>{{ $f->nome }}</td>
       <td>{{ $f->login }}</td>
      <td>
-      <!--<a class="btn btn-warning" href="#">Alterar</a>  {{ route('#', ['id' => $f->id]) }} -->
-      <!-- <a class="btn btn-danger" href="#" onclick="excluir({{ $f->id }})"> Excluir</a>-->
-      <!--<a class="btn btn-success" href="#">Vendas</a> {{ route('#', ['id' => $f->id]) }}-->
+      <a class="btn btn-warning" href="{{ route('#', ['id' => $f->id]) }} ">Alterar</a>  
+      <a class="btn btn-danger" href="#" onclick="excluir({{ $f->id }})"> Excluir</a>
+      <a class="btn btn-success" href="{{ route('#', ['id' => $f->id]) }}">Vendas</a> 
      </td>
     </tr>
     @endforeach
   </tbody>
 
 </table>
-<!-- <a class="btn btn-primary" href="{{route('cadastro_funcionario')}}">Cadastrar novo</a> -->
+<a class="btn btn-primary" href="{{route('cadastro_funcionario')}}">Cadastrar novo</a>
 
 <script>
   function excluir(id){
