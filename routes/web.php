24c3b5d543da9374154289f930131a8b2a0ce4fd
henriquefantin <henrique.fantin.ns@gmail.com>
2020-05-31 17:49:53 -0300

    037 TG01 31.05.20:17.49

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,29 @@ Route::middleware(['auth'])->group(function(){
 	Route::get('/cliente/cadastro', 'ClienteController@telaCadastro')->name('cadastro_cliente');
 	Route::get('/cliente/listar', 'ClienteController@listar')->name('listar_cliente');
 
+	/* Fornecedores */
+	Route::get('/fornecedor/adicionar', 'FornecedorController@adicionar')->name('fornecedor_add');
+	Route::get('/fornecedor/cadastro', 'FornecedorController@telaCadastro')->name('cadastro_fornecedor');
+	Route::get('/fornecedor/listar', 'FornecedorController@listar')->name('listar_fornecedor');
 
 	/* Funcionario */
 	Route::get('/funcionario/adicionar', 'FuncionarioController@adicionar')->name('funcionario_add');	
 	Route::get('/funcionario/listar', 'FuncionarioController@listar')->name('listar_funcionarios');
+
+	/* Vendas */
+	Route::get('/venda/cadastro', 'VendaController@telaCadastro')->name('venda_cadastro');
+	Route::post('/venda/adicionar', 'VendaController@adicionar')->name('venda_add');
+	Route::get('/venda/{id}/itens/novo', 'VendaController@telaAdicionarItem')->name('vendas_item_novo');
+	Route::post('/venda/{id}/itens/adicionar', 'VendaController@adicionarItem')->name('vendas_item_add');
+	Route::get('/venda/{id}/itens/remover/{id_pivot}', 'VendaController@excluirItem')->name('vendas_item_delete');
+	Route::get('/venda/{id}/itens', 'VendaController@itensVenda')->name('vendas_itens');
+	Route::get('/venda/listar', 'VendaController@listar')->name('venda_listar');
+	Route::get('/venda/usuario/{id}', 'VendaController@vendasPorUsuario')->name('vendas_usuario');
+	
+	/* Produtos */
+	Route::get('/produto/cadastro', 'ProdutoController@telaCadastro')->name('produto_cadastro');
+	Route::post('/produto/adicionar', 'ProdutoController@adicionar')->name('produto_add');
+	Route::get('/produto/listar', 'ProdutoController@listar')->name('produtos_listar');
 
 	Route::middleware(['admin'])->group(function(){
 		/* Funcionario */
